587f71ff43d3c0958a7751388ecbce9915c21b19
Keith <kayceecode@gmail.com>
2024-08-04 17:37:12 -0400

    Added Models & EntityFramework to .Data

---
+++ EFCore.Data/Models/League.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCore.Data.Models;
+
+public partial class League
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public virtual ICollection<Team> Teams { get; set; } = new List<Team>();
+}
---
+++ EFCore.Data/Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCore.Data.Models;
+
+public partial class Team
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int LeagueId { get; set; }
+
+    public virtual League League { get; set; }
+}
